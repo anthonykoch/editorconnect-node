3977d529d5ff94a9aa62d98ce2b5ef9233116095
anthonykoch <anthonykoch@users.noreply.github.com>
2018-03-11 17:04:21 -0700

    Fix lint

--- dist/messages.js
+++ dist/messages.js
@@ -62,7 +62,10 @@ exports.reply = reply;
 var isReply = function isReply(message) {
   var _message$origin, _message$to, _message$to2;
 
-  return message && isFilled(message.id) && (0, _isLength.default)(message.iid) && isFilled(message === null || message === void 0 ? void 0 : (_message$origin = message.origin) === null || _message$origin === void 0 ? void 0 : _message$origin.id) && (0, _isLength.default)(message.part) && typeof message.done === 'boolean' && isFilled(message === null || message === void 0 ? void 0 : (_message$to = message.to) === null || _message$to === void 0 ? void 0 : _message$to.id) && isFilled(message === null || message === void 0 ? void 0 : (_message$to2 = message.to) === null || _message$to2 === void 0 ? void 0 : _message$to2.event);
+  return message && isFilled(message.id) && (0, _isLength.default)(message.iid) && // eslint-disable-next-line no-undef
+  isFilled(message === null || message === void 0 ? void 0 : (_message$origin = message.origin) === null || _message$origin === void 0 ? void 0 : _message$origin.id) && (0, _isLength.default)(message.part) && typeof message.done === 'boolean' && // eslint-disable-next-line no-undef
+  isFilled(message === null || message === void 0 ? void 0 : (_message$to = message.to) === null || _message$to === void 0 ? void 0 : _message$to.id) && // eslint-disable-next-line no-undef
+  isFilled(message === null || message === void 0 ? void 0 : (_message$to2 = message.to) === null || _message$to2 === void 0 ? void 0 : _message$to2.event);
 };
 /**
  * Creates a call message.
@@ -92,7 +95,8 @@ exports.call = call;
 var isCall = function isCall(message) {
   var _message$origin2;
 
-  return message && message.type === 'call' && isFilled(message.id) && (0, _isLength.default)(message.iid) && isFilled(message.event) && message.hasOwnProperty('payload') && isFilled((_message$origin2 = message.origin) === null || _message$origin2 === void 0 ? void 0 : _message$origin2.id);
+  return message && message.type === 'call' && isFilled(message.id) && (0, _isLength.default)(message.iid) && isFilled(message.event) && message.hasOwnProperty('payload') && // eslint-disable-next-line no-undef
+  isFilled((_message$origin2 = message.origin) === null || _message$origin2 === void 0 ? void 0 : _message$origin2.id);
 };
 /**
  * Creates a handshake message, which is used when a client connects to the server.
@@ -121,7 +125,8 @@ exports.handshake = handshake;
 var isHandshake = function isHandshake(message) {
   var _message$origin3;
 
-  return message && message.type === 'handshake' && isFilled(message.id) && isFilled((_message$origin3 = message.origin) === null || _message$origin3 === void 0 ? void 0 : _message$origin3.id) && message.hasOwnProperty('payload');
+  return message && message.type === 'handshake' && isFilled(message.id) && // eslint-disable-next-line no-undef
+  isFilled((_message$origin3 = message.origin) === null || _message$origin3 === void 0 ? void 0 : _message$origin3.id) && message.hasOwnProperty('payload');
 };
 /**
  * Creates a handshake accept.
@@ -151,7 +156,9 @@ exports.handshakeAccept = handshakeAccept;
 var isHandshakeAccept = function isHandshakeAccept(message) {
   var _message$origin4, _message$to3;
 
-  return message && message.type === 'handshake-accept' && isFilled(message.id) && isFilled((_message$origin4 = message.origin) === null || _message$origin4 === void 0 ? void 0 : _message$origin4.id) && isFilled((_message$to3 = message.to) === null || _message$to3 === void 0 ? void 0 : _message$to3.id) && message.hasOwnProperty('payload');
+  return message && message.type === 'handshake-accept' && isFilled(message.id) && // eslint-disable-next-line no-undef
+  isFilled((_message$origin4 = message.origin) === null || _message$origin4 === void 0 ? void 0 : _message$origin4.id) && // eslint-disable-next-line no-undef
+  isFilled((_message$to3 = message.to) === null || _message$to3 === void 0 ? void 0 : _message$to3.id) && message.hasOwnProperty('payload');
 };
 /**
  * Returns true if the message conforms to the api.
